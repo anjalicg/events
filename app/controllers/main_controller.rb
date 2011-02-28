5b57ddb4a8e48af48907acf9970953467731bdd9
Anjali C. G <anjalicg2001@gmail.com>
2011-03-01 02:19:44 +0530

    addding alpha

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,11 +4,15 @@ require 'strscan'
 class MainController < ApplicationController
   def alpha
     trusted_list = ["anjalicg2001@gmail.com", "sureshg.g@gmail.com"]
-    if trusted_list.include?(params[:alpha][:email].downcase)
-      puts "USer is in trusted list"
-      else
-        puts "user not in trusted list"
+    case request.method
+      when :post:      
+      if trusted_list.include?(params[:alpha][:email].downcase)
+        puts "USer is in trusted list"
+        else
+          puts "user not in trusted list"
+        end
       end
+      
       
   end
   
