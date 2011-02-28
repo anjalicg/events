20b18f2cfdabd92687602009ba18477cfea10154
Anjali C. G <anjalicg2001@gmail.com>
2011-03-01 01:58:56 +0530

    changed joinme to eventzy

--- app/models/joinme_mailer.rb
+++ app/models/joinme_mailer.rb
@@ -3,32 +3,32 @@ class JoinmeMailer < ActionMailer::Base
 
   def newuser(user,code,sent_at = Time.now)
 #    subject    'JoinmeMailer#newuser: New user account activation'
-    @subject=    'JoinMe new user account activation email'
+    @subject=    'Eventzy new user account activation email'
     @recipients= user.email
     @from=       "no-reply@evetzy.heroku.com"
     @sent_on=    sent_at
     
-    @body=       "Hi, \n Welcome to JoinMe. Click the following link to activate your account and find a companion for your events http://eventzy.heroku.com/user/activate/?newauth=#{code}&email=#{user.email}"
+    @body=       "Hi, \n Welcome to Eventzy. Click the following link to activate your account and find a companion for your events http://eventzy.heroku.com/user/activate/?newauth=#{code}&email=#{user.email}"
   end
 
   def eventedit(responder,event,changes,sent_at = Time.now)
-    @subject=    'Event edit notification from JoinMe'
+    @subject=    'Event edit notification from Eventzy'
     @recipients= responder[2]
     @from=       "no-reply@evetzy.heroku.com"
     sent_on    sent_at
     
-    @body=       "Dear #{responder[1] },\n  The following details regarding the Event your were interested in has changed. \n #{changes}\n You can see the changes by visiting the link http://eventzy.heroku.com/event/show/#{event.id}. If the changes do not suit you, then you can cancel your participation by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} \n Thank you for using JoinME!\n -The JoinMe team."
+    @body=       "Dear #{responder[1] },\n  The following details regarding the Event your were interested in has changed. \n #{changes}\n You can see the changes by visiting the link http://eventzy.heroku.com/event/show/#{event.id}. If the changes do not suit you, then you can cancel your participation by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} \n Thank you for using Eventzy!\n -The Eventzy team."
 
 	puts "Sent mail to #{responder[1]},id=#{responder[0]}, at #{responder[2]}"
   end
 
   def eventdelete(responder,event,sent_at = Time.now)
-    @subject=    'Event delete notification from JoinMe'
+    @subject=    'Event delete notification from Eventzy'
     @recipients= responder[2]
     @from=       "no-reply@evetzy.heroku.com"
     sent_on    sent_at
     
-    @body=       "Dear #{responder[1] },\n  This is to notify you that the event your were interested has been deleted. \n You might want to find new events to attend by visiting the link http://eventzy.heroku.com. You can check the status of your upcoming events by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} . \n Thank you for using JoinME!\n -The JoinMe team."
+    @body=       "Dear #{responder[1] },\n  This is to notify you that the event your were interested has been deleted. \n You might want to find new events to attend by visiting the link http://eventzy.heroku.com. You can check the status of your upcoming events by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} . \n Thank you for using Eventzy!\n -The Eventzy team."
 
 	puts "Sent mail to #{responder[1]},id=#{responder[0]}, at #{responder[2]}"
 
@@ -41,26 +41,26 @@ class JoinmeMailer < ActionMailer::Base
     @from=       "no-reply@evetzy.heroku.com"
     sent_on    sent_at
     
-    @body= "Dear #{event.user.display_name}, \n You have a new response for your event '#{event.title}' from '#{user.display_name}'. \n You can see the user profile by visiting http://eventzy.heroku.com/user/user_home/#{user.id}. \n You can accept the response by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{event.user.display_name}, \n You have a new response for your event '#{event.title}' from '#{user.display_name}'. \n You can see the user profile by visiting http://eventzy.heroku.com/user/user_home/#{user.id}. \n You can accept the response by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using Eventzy!. \n Hope you have a great time.\n -The Eventzy team."
   end
 def deleteresponse(user,event,sent_at = Time.now)
     @subject=    'Participation cancellation notification for your event'
     @recipients= event.user.email
     @from=       "no-reply@evetzy.heroku.com"
     sent_on    sent_at
-    @body= "Dear #{event.user.display_name}, \n The user '#{user.display_name}' has cancelled participation for event '#{event.title}'.  \n You can see other responses for your event by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{event.user.display_name}, \n The user '#{user.display_name}' has cancelled participation for event '#{event.title}'.  \n You can see other responses for your event by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using Eventzy!. \n Hope you have a great time.\n -The Eventzy team."
   end
 def acceptresponse(user,event,sent_at = Time.now)
     @subject=    'Your event participation request has been accepted'
     @recipients= user.email
     @from=       "no-reply@evetzy.heroku.com"
     sent_on    sent_at
-    @body= "Dear #{user.display_name}, \n Your request to participate in the event '#{event.title}' has been accepted.  \nYou can see the owner profile by visiting http://eventzy.heroku.com/user/user_home/#{event.user.id}.\n You can see the event details by visiting http://eventzy.heroku.com/event/show/#{event.id}\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{user.display_name}, \n Your request to participate in the event '#{event.title}' has been accepted.  \nYou can see the owner profile by visiting http://eventzy.heroku.com/user/user_home/#{event.user.id}.\n You can see the event details by visiting http://eventzy.heroku.com/event/show/#{event.id}\n Thank you for using Eventzy!. \n Hope you have a great time.\n -The Eventzy team."
   end
     def invite_frnd(invite,sent_at = Time.now)
 
 puts "Inside invite_frnd #{invite.mailtext.length}"
-    @subject=   "Your friend #{invite.user.display_name} has invited you to join JoinMe"
+    @subject=   "Your friend #{invite.user.display_name} has invited you to join Eventzy"
 puts "After subject"
     @recipients= invite.email
 puts "After email"
@@ -70,16 +70,16 @@ puts "After email"
 puts "Just before if loop"
     if invite.mailtext.length > 0
 puts "mail text is true #{invite.name}, #{invite.mailtext}"
-	@body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. Please see their message below\n\n\n" + invite.mailtext + "\n\n\nVisit http://eventzy.heroku.com to try out our services today.\n Best Regards, \n -The JoinMe team"
+	@body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join Eventzy. Please see their message below\n\n\n" + invite.mailtext + "\n\n\nVisit http://eventzy.heroku.com to try out our services today.\n Best Regards, \n -The Eventzy team"
 else
 puts "No mail text present #{invite.name}"
-    @body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. \nVisit http://eventzy.heroku.com to try out our services today.\n \n Thank you for using Join Me! \n - The Join Me team"
+    @body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join Eventzy. \nVisit http://eventzy.heroku.com to try out our services today.\n \n Thank you for using Join Me! \n - The Join Me team"
   end
 end
 
 
     def recoverpassword(user,code,sent_at = Time.now)
-    @subject=   'Reset Password link from JoinMe'
+    @subject=   'Reset Password link from Eventzy'
     @recipients= user.email
     @from=       "no-reply@evetzy.heroku.com"
     @sent_on=    sent_at
