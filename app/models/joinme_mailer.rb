2e5b21ed44570d9030f638d662309c5a074a1293
Anjali C. G <anjalicg2001@gmail.com>
2011-03-01 00:48:18 +0530

    email url changed

--- app/models/joinme_mailer.rb
+++ app/models/joinme_mailer.rb
@@ -8,7 +8,7 @@ class JoinmeMailer < ActionMailer::Base
     @from=       'anjalicg@gainspan.com'
     @sent_on=    sent_at
     
-    @body=       "Hi, \n Welcome to JoinMe. Click the following link to activate your account and find a companion for your events http://anjalicg2001.dynalias.com/user/activate/?newauth=#{code}&email=#{user.email}"
+    @body=       "Hi, \n Welcome to JoinMe. Click the following link to activate your account and find a companion for your events http://eventzy.heroku.com/user/activate/?newauth=#{code}&email=#{user.email}"
   end
 
   def eventedit(responder,event,changes,sent_at = Time.now)
@@ -17,7 +17,7 @@ class JoinmeMailer < ActionMailer::Base
     @from=       "anjalicg@gainspan.com"
     sent_on    sent_at
     
-    @body=       "Dear #{responder[1] },\n  The following details regarding the Event your were interested in has changed. \n #{changes}\n You can see the changes by visiting the link http://anjalicg2001.dynalias.com/event/show/#{event.id}. If the changes do not suit you, then you can cancel your participation by visiting the link http://anjalicg2001.dynalias.com/response/list_by_responder/?user_id=#{responder[0]} \n Thank you for using JoinME!\n -The JoinMe team."
+    @body=       "Dear #{responder[1] },\n  The following details regarding the Event your were interested in has changed. \n #{changes}\n You can see the changes by visiting the link http://eventzy.heroku.com/event/show/#{event.id}. If the changes do not suit you, then you can cancel your participation by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} \n Thank you for using JoinME!\n -The JoinMe team."
 
 	puts "Sent mail to #{responder[1]},id=#{responder[0]}, at #{responder[2]}"
   end
@@ -28,7 +28,7 @@ class JoinmeMailer < ActionMailer::Base
     @from=       "anjalicg@gainspan.com"
     sent_on    sent_at
     
-    @body=       "Dear #{responder[1] },\n  This is to notify you that the event your were interested has been deleted. \n You might want to find new events to attend by visiting the link http://anjalicg2001.dynalias.com. You can check the status of your upcoming events by visiting the link http://anjalicg2001.dynalias.com/response/list_by_responder/?user_id=#{responder[0]} . \n Thank you for using JoinME!\n -The JoinMe team."
+    @body=       "Dear #{responder[1] },\n  This is to notify you that the event your were interested has been deleted. \n You might want to find new events to attend by visiting the link http://eventzy.heroku.com. You can check the status of your upcoming events by visiting the link http://eventzy.heroku.com/response/list_by_responder/?user_id=#{responder[0]} . \n Thank you for using JoinME!\n -The JoinMe team."
 
 	puts "Sent mail to #{responder[1]},id=#{responder[0]}, at #{responder[2]}"
 
@@ -41,21 +41,21 @@ class JoinmeMailer < ActionMailer::Base
     @from=       "anjalicg@gainspan.com"
     sent_on    sent_at
     
-    @body= "Dear #{event.user.display_name}, \n You have a new response for your event '#{event.title}' from '#{user.display_name}'. \n You can see the user profile by visiting http://anjalicg2001.dynalias.com/user/user_home/#{user.id}. \n You can accept the response by visiting http://anjalicg2001.dynalias.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{event.user.display_name}, \n You have a new response for your event '#{event.title}' from '#{user.display_name}'. \n You can see the user profile by visiting http://eventzy.heroku.com/user/user_home/#{user.id}. \n You can accept the response by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
   end
 def deleteresponse(user,event,sent_at = Time.now)
     @subject=    'Participation cancellation notification for your event'
     @recipients= event.user.email
     @from=       "anjalicg@gainspan.com"
     sent_on    sent_at
-    @body= "Dear #{event.user.display_name}, \n The user '#{user.display_name}' has cancelled participation for event '#{event.title}'.  \n You can see other responses for your event by visiting http://anjalicg2001.dynalias.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{event.user.display_name}, \n The user '#{user.display_name}' has cancelled participation for event '#{event.title}'.  \n You can see other responses for your event by visiting http://eventzy.heroku.com/response/list_by_event?event_id=#{event.id}.\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
   end
 def acceptresponse(user,event,sent_at = Time.now)
     @subject=    'Your event participation request has been accepted'
     @recipients= user.email
     @from=       "anjalicg@gainspan.com"
     sent_on    sent_at
-    @body= "Dear #{user.display_name}, \n Your request to participate in the event '#{event.title}' has been accepted.  \nYou can see the owner profile by visiting http://anjalicg2001.dynalias.com/user/user_home/#{event.user.id}.\n You can see the event details by visiting http://anjalicg2001.dynalias.com/event/show/#{event.id}\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
+    @body= "Dear #{user.display_name}, \n Your request to participate in the event '#{event.title}' has been accepted.  \nYou can see the owner profile by visiting http://eventzy.heroku.com/user/user_home/#{event.user.id}.\n You can see the event details by visiting http://eventzy.heroku.com/event/show/#{event.id}\n Thank you for using JoinME!. \n Hope you have a great time.\n -The JoinMe team."
   end
     def invite_frnd(invite,sent_at = Time.now)
 
@@ -70,10 +70,10 @@ puts "After email"
 puts "Just before if loop"
     if invite.mailtext.length > 0
 puts "mail text is true #{invite.name}, #{invite.mailtext}"
-	@body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. Please see their message below\n\n\n" + invite.mailtext + "\n\n\nVisit http://anjalicg2001.dynalias.com to try out our services today.\n Best Regards, \n -The JoinMe team"
+	@body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. Please see their message below\n\n\n" + invite.mailtext + "\n\n\nVisit http://eventzy.heroku.com to try out our services today.\n Best Regards, \n -The JoinMe team"
 else
 puts "No mail text present #{invite.name}"
-    @body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. \nVisit http://anjalicg2001.dynalias.com to try out our services today.\n \n Thank you for using Join Me! \n - The Join Me team"
+    @body="Dear #{invite.name}, \n  One of your friends #{invite.user.display_name} has invited your to join JoinMe. \nVisit http://eventzy.heroku.com to try out our services today.\n \n Thank you for using Join Me! \n - The Join Me team"
   end
 end
 
@@ -83,7 +83,7 @@ end
     @recipients= user.email
     @from=       'anjalicg@gainspan.com'
     @sent_on=    sent_at
-    @body=	"Dear #{user.display_name}, \nWe have received your request to reset your password. \n click the following link to visit the page to reset your password.\n http://anjalicg2001.dynalias.com/user/reset_password/?resetcode=#{code}&email=#{user.email} . If you did not request to reset your password, then ignore this mail and you can continue using your old password to access our website. \n Thank you for using Join Me! \n - The Join Me team"
+    @body=	"Dear #{user.display_name}, \nWe have received your request to reset your password. \n click the following link to visit the page to reset your password.\n http://eventzy.heroku.com/user/reset_password/?resetcode=#{code}&email=#{user.email} . If you did not request to reset your password, then ignore this mail and you can continue using your old password to access our website. \n Thank you for using Join Me! \n - The Join Me team"
   end
 
 end
